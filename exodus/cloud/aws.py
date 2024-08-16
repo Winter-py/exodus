b10815e60816554e8622fb9d495cf1273268652e
Steven Owatemi <isosteven@gmail.com>
2024-08-16 15:47:30 +0100

    try/catch

--- exodus/cloud/aws.py
+++ exodus/cloud/aws.py
@@ -1,12 +1,16 @@
 import boto3
 from botocore.exceptions import ClientError
 
-def upload_to_s3(region,bucket_name, file_path, object_name=None):
-    s3 = boto3.client('s3', region_name=region)
+def upload_to_s3(bucket_name, file_path, object_name=None):
+    s3 = boto3.client('s3')
     if object_name is None:
         object_name = file_path
-    
-    s3.upload_file(file_path, bucket_name, object_name)
+    try:
+        s3.upload_file(file_path, bucket_name, object_name)
+    except ClientError as e:
+        print(e)
+        return False
+    return True
     
     
     
@@ -18,8 +22,9 @@ def create_s3_bucket(region,bucket_name):
                            'LocationConstraint': region,
                         }
                           )
-    except Exception as e:
+    except ClientError as e:
         print("Response:",e)
-        return e
+        return False
+    return True
     
     
