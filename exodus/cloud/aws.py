f94548e294d931d4172020c7c42fb09131f1b816
Steven Owatemi <isosteven@gmail.com>
2024-08-16 10:19:23 +0100

    upload function

---
+++ exodus/cloud/aws.py
@@ -0,0 +1,8 @@
+import boto3
+
+def upload_to_s3(bucket_name, file_path, object_name=None):
+    s3 = boto3.client('s3')
+    if object_name is None:
+        object_name = file_path
+    
+    s3.upload_file(file_path, bucket_name, object_name)
