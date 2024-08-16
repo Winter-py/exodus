c59fe52fb5727e1327dcd5e5a6f4d6d2fc65dd0f
Steven Owatemi <isosteven@gmail.com>
2024-08-16 10:15:50 +0100

    carried away

--- exodus/backup/files.py
+++ exodus/backup/files.py
@@ -12,24 +12,3 @@ def backup_files(file_paths, backup_dir):
             shutil.copy(file_path, backup_dir)
         else:
             print(f"File {file_path} not found.")
-
-# backup_cli/backup/database.py
-
-def backup_database(config):
-    db_type = config['database']['type']
-    
-    if db_type == 'mysql':
-        backup_mysql_database(config['database'])
-    # Add support for more databases here
-    
-    if db_type == 'mssql':
-        backup_mssql_database(config['database'])
-
-
-def backup_mysql_database(db_config):
-    # Implement MySQL backup logic using subprocess to run mysqldump
-    pass
-
-def backup_mssql_database(db_config):
-    # Implement MySQL backup logic using subprocess to run MSSQLSERVER
-    pass
