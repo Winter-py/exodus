54f45833c11cb13f5146458ab724e860393cf315
Steven Owatemi <isosteven@gmail.com>
2024-10-11 14:48:32 +0100

    region

--- tests/test_aws.py
+++ tests/test_aws.py
@@ -1,12 +1,12 @@
 import unittest
 from unittest.mock import patch
 import boto3
-from moto import mock_s3
+from moto import mock_aws
 from exodus.cloud.aws import upload_to_s3, create_s3_bucket  # Replace 'my_module' with your actual module name
 
 class TestS3Functions(unittest.TestCase):
 
-    @mock_s3
+    @mock_aws
     def test_create_s3_bucket(self):
         region = "us-west-1"
         bucket_name = "test-bucket"
@@ -20,21 +20,21 @@ class TestS3Functions(unittest.TestCase):
         buckets = [bucket['Name'] for bucket in response['Buckets']]
         self.assertIn(bucket_name, buckets)
 
-    @mock_s3
+    @mock_aws
     def test_upload_to_s3(self):
-        region = "us-west-1"
+        region = "eu-west-2"
         bucket_name = "test-bucket"
-        file_path = "test.txt"
+        file_path = "C:\\Backup\\test.txt"
         object_name = "uploaded_test.txt"
 
         # Create the bucket first
         s3 = boto3.client('s3', region_name=region)
-        s3.create_bucket(Bucket=bucket_name)
+        # s3.create_bucket(region, Bucket=bucket_name)
 
         # Mock the open function to simulate a file
         with patch("builtins.open", unittest.mock.mock_open(read_data="data")) as mock_file:
             # Call the function to upload the file
-            upload_to_s3(region, bucket_name, file_path, object_name)
+            upload_to_s3(bucket_name, file_path, object_name)
 
         # Assert that the file was uploaded
         response = s3.list_objects_v2(Bucket=bucket_name)
